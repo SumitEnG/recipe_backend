7148361001ae03ef4bfae4a11835378718b4bcb7
SumitEnG <kumarsukdev404@gmail.com>
2023-06-06 21:08:27 +0530

    ..

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(express.json());
 require("./startup/db")();
 require("./startup/routes")(app);
 
+app.get("/", (req, res) => {
+  res.send("<h1>working fine</h1>");
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
